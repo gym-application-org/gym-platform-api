716e53e2cee51512f784473379412f125d897065
eren karabulut <erenkrabulut@gmail.com>
2026-02-20 16:59:14 +0300

    Added new general operation claims.

--- src/corePackages/Core.Security/Constants/GeneralOperationClaims.cs
+++ src/corePackages/Core.Security/Constants/GeneralOperationClaims.cs
@@ -3,4 +3,7 @@
 public static class GeneralOperationClaims
 {
     public const string Admin = "Admin";
+    public const string Owner = "Owner";
+    public const string Staff = "Staff";
+    public const string Member = "Member";
 }
